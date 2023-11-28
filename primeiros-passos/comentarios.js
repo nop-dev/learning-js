b6d7599efb3be4ab7e3b576850a0bafad1411a2d
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2023-11-27 21:42:09 -0300

    feat: :sparkles: Comentários em JavaScript

---
+++ primeiros-passos/comentarios.js
@@ -0,0 +1,5 @@
+console.log('Começando os meus estudo em JavaScript') // dá pra fazer comentários também ao lado do código
+
+// Para ignorar uma linha de código em JS é só usar "//"
+
+/* Para comentar em bloco é só usar: /* */
